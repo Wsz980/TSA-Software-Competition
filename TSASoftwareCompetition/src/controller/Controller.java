ce510c9768438e655b82b0a21231e64f027196a3
theDragonFire <bhbochikens@gmail.com>
2016-12-22 20:41:09 -0700

    Created controller/model/view format in project

---
+++ TSASoftwareCompetition/src/controller/Controller.java
@@ -0,0 +1,15 @@
+package controller;
+
+public class Controller {
+	
+	Controller() {
+		
+	}
+	
+	public void run() {
+		
+		//Instructions for the program here
+		
+	}
+	
+}
---
+++ TSASoftwareCompetition/src/controller/Runner.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class Runner {
+	
+	public static void main(String args[]) {
+		
+		Controller controller = new Controller();
+		controller.run();
+		
+	}
+	
+}
